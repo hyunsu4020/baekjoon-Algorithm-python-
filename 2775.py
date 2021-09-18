463cf21ebd45475c52390a52f85d93d37325d15f
hyunsu4020 <76948939+hyunsu4020@users.noreply.github.com>
2021-09-18 23:48:29 +0900

    Create 2775.py

---
+++ 2775.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for i in range(T):
+    k = int(input())
+    n = int(input())
+    people = [i for i in range(1, n+1)]
+
+    for x in range(k):
+        for y in range(1, n):
+            people[y] += people[y-1]
+        print(people[-1])
