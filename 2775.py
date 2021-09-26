045462420f305d7f971e2feecd441bef76891881
hyunsu4020 <76948939+hyunsu4020@users.noreply.github.com>
2021-09-26 15:32:42 +0900

    Update 2775.py

--- 2775.py
+++ 2775.py
@@ -1,11 +1,10 @@
-T = int(input())
+t = int(input())
 
-for i in range(T):
+for _ in range(t):
     k = int(input())
     n = int(input())
     people = [i for i in range(1, n+1)]
-
-    for x in range(k):
-        for y in range(1, n):
-            people[y] += people[y-1]
-        print(people[-1])
+    for _ in range(k):
+        for m in range(1, n):
+            people[m] += people[m-1]
+    print(people[n-1])
