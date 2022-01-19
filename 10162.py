c1b70bea3d98c5de19a0e81accf658c544fe20ac
hyunsu4020 <76948939+hyunsu4020@users.noreply.github.com>
2022-01-19 22:44:49 +0900

    전자레인지
    전자레인지

---
+++ 10162.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+if t % 2 != 0:
+    print(-1)
+else:
+    a = 0
+    b = 0
+    c = 0
+    a = t // 300
+    b = (t % 300) // 60
+    c = (t % 300) % 60 // 10
+    print(a, b, c)
