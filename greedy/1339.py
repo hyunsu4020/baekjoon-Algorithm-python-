8955dfc2e48fab330cffc65d97e919f850772f0d
hyunsu4020 <76948939+hyunsu4020@users.noreply.github.com>
2022-01-25 00:51:50 +0900

    단어 수학
    문제: 민식이는 수학학원에서 단어 수학 문제를 푸는 숙제를 받았다.
    단어 수학 문제는 N개의 단어로 이루어져 있으며, 각 단어는 알파벳 대문자로만 이루어져 있다. 이때, 각 알파벳 대문자를 0부터 9까지의 숫자 중 하나로 바꿔서 N개의 수를 합하는 문제이다. 같은 알파벳은 같은 숫자로 바꿔야 하며, 두 개 이상의 알파벳이 같은 숫자로 바뀌어지면 안 된다.
    예를 들어, GCF + ACDEB를 계산한다고 할 때, A = 9, B = 4, C = 8, D = 6, E = 5, F = 3, G = 7로 결정한다면, 두 수의 합은 99437이 되어서 최대가 될 것이다.
    N개의 단어가 주어졌을 때, 그 수의 합을 최대로 만드는 프로그램을 작성하시오.
    
    입력: 첫째 줄에 단어의 개수 N(1 ≤ N ≤ 10)이 주어진다. 둘째 줄부터 N개의 줄에 단어가 한 줄에 하나씩 주어진다. 단어는 알파벳 대문자로만 이루어져있다. 모든 단어에 포함되어 있는 알파벳은 최대 10개이고, 수의 최대 길이는 8이다. 서로 다른 문자는 서로 다른 숫자를 나타낸다.
    
    출력: 첫째 줄에 주어진 단어의 합의 최댓값을 출력한다.

--- greedy/1339.py
+++ greedy/1339.py
@@ -1,16 +1,28 @@
 import sys
 
-input = sys.stdin.realin()
+input = sys.stdin.readline
 n = int(input())
 
-alpha = []          # 단어 저장
-alpha_dict = {}     # 단어 내의 알파벳 별로 수를 저장할 딕셔너리
+alpha_dict = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0,
+            'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0,
+            'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}         # 단어 내의 알파벳 별로 수를 저장할 딕셔너리
+alpha_list = []     # 단어 저장
 numList = []        # 수를 저장할 리스트
 
-for i in range(n):  # 단어 입력
-    alpha.append(input().rstrip())      # rstrip(): 오른쪽 공백 제거
+sum = 0
+for _ in range(n):  # 단어 입력
+    numList.append(input().rstrip())      # rstrip(): 오른쪽 공백 제거
 
-for i in range(n):
-    for j in range(len(alpha[i]))       #  입력받은 단어의 길이만큼 실행
-        if alpha[i][j] in alpha_dict:   #  alpha이 이미 dict에 있으면
-            alpha_dict[alpha[i][j]] += 10 ** (len(alpha[i])-j-1)
+for i in numList:
+    for j in range(len(i)):
+        alpha_dict[i[j]] += 10 ** (len(i)-j-1)
+
+for i in alpha_dict.values():   # dict에 저장된 수들을 모두 리스트에 추가
+    if i > 0:
+        alpha_list.append(i)
+
+sorted_list = sorted(alpha_list, reverse=True)      # 내림 차순 정렬
+for i in range(len(sorted_list)):
+    sum += sorted_list[i] * (9-i)   # 첫 부분에 9를 * 내려갈 수록 -1씩 다운해서 *
+
+print(sum)
